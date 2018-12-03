c27df5ca05568dcc12d2fb74f037bb5e249459ec
Matias Medina <matias.medina.s2@gmail.com>
2018-12-03 20:44:45 -0300

    aplicando redux en react

--- resources/assets/js/components/index.js
+++ resources/assets/js/components/index.js
@@ -2,4 +2,11 @@ import React from 'react'
 import { render } from 'react-dom'
 import TabPanel from "./TabPanel";
 
+import store from "../store/index";
+import { addArticle } from "../actions/index";
+
+window.store = store;
+window.addArticle = addArticle;
+
+
 render(<TabPanel/>, document.getElementById('root'))
